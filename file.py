a22ca5094fbca75c6ffb5928fb0744c8c5e9fabf
de-schieb <de.schieb@gmail.com>
2020-11-22 20:04:48 +0100

    first commit

---
+++ file.py
@@ -0,0 +1,16 @@
+class Character:
+    def __init__(self,name,age,hp):
+        self.name = name
+        self.age = age
+        self.hp = hp
+    #def kämpfen(hp)
+    def sagHallo(self,String):
+        print("Hello I m " + self.name + String)    
+
+class Monster:
+    def __init__(self,name):
+        self.name
+
+print("Willkommen in der Kluft der Beschwörer")
+name = input("Geben Sie Ihren Charaktername ein: ")
+
